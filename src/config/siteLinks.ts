658ac7eea9fcc44ca4a8653883afa96c220863e1
Aaron Nayki <138834250+slasherthunder@users.noreply.github.com>
2026-06-22 14:08:07 -0700

    Add Accessibility Surfer Chrome extension link in site footer.
    Links visitors to the published Web Store listing alongside the on-site accessibility toolbar.

--- src/config/siteLinks.ts
+++ src/config/siteLinks.ts
@@ -4,6 +4,9 @@
  */
 export const HISTORICAL_SOCIETY_URL = 'https://delmarhistoricalsociety.org/';
 
+export const ACCESSIBILITY_SURFER_EXTENSION_URL =
+	'https://chromewebstore.google.com/detail/accessibility-surfer/pccmbliammnfaklpblehkonmhcdnedhn';
+
 const ZEFFY_FALLBACK = 'https://www.zeffy.com/en-US/ticketing/del-mar-surf-reunion';
 
 export function getZeffyDonationUrl(): string {
